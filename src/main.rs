ac4b6232c4217ba321ba9caaa9a3fd215101b7c7
Simon Whitehead <chemnova@gmail.com>
2016-01-20 22:56:06 +1100

    Added some extra output to StdOut

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ extern crate rustyard;
 fn main() {
     let shunting_yard = rustyard::ShuntingYard::new("2 + 4 * 3");
 
-    println!("{}", shunting_yard.to_string());
+    println!("Input is: 2 + 4 * 3");
+    println!("Shunting Yard result: {}", shunting_yard.to_string());
 }
