5d0adba785f548bad1edce3b5348ec80b22908ee
Simon Whitehead <chemnova@gmail.com>
2016-01-23 18:09:04 +1100

    Added command line argument support

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,40 @@
 extern crate rustyard;
 
+use std::env;
+use std::slice;
+
 fn main() {
-    let input = "(2 + 4) * 3)";
-    let shunting_yard = rustyard::ShuntingYard::new(input);
+    let mut args = env::args().collect::<Vec<String>>();
 
-    println!("Input is: {}", input);
+    if args.len() > 1 {
+        args.remove(0);
+        let input = join_args(args);
+        let shunting_yard = rustyard::ShuntingYard::new(&input[..]);
 
-    if shunting_yard.errors.len() > 0 {
-        println!("Errors:");
-        for err in shunting_yard.errors {
-            println!("ERR: {}", err);
+        println!("Input is: {}", input);
+
+        if shunting_yard.errors.len() > 0 {
+            println!("Errors:");
+            for err in shunting_yard.errors {
+                println!("ERR: {}", err);
+            }
+        } else {
+            println!("Lexer result: {}", shunting_yard.to_string_ast());
+            println!("Shunting Yard result: {}", shunting_yard.to_string());
+            println!("Equation equals: {}", shunting_yard.calculate());
         }
     } else {
-        println!("Lexer result: {}", shunting_yard.to_string_ast());
-        println!("Shunting Yard result: {}", shunting_yard.to_string());
-        println!("Equation equals: {}", shunting_yard.calculate());
+        println!("Please supply an expression");
     }
+}
+
+fn join_args(args: Vec<String>) -> String {
+    let mut result = String::new();
+
+    for arg in args {
+        result.push_str(&arg[..]);
+        result.push_str(" ");
+    }
+
+    result
 }
