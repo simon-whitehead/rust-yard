7df43ea5662f7f01dea4626e2210b4f02e0534e9
Simon Whitehead <chemnova@gmail.com>
2016-01-28 23:36:39 +1100

    Indenting cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,23 @@ fn main() {
 
             println!("Input is: {}", input);
 
-                    if shunting_yard.errors.len() > 0 {
-                        println!("Errors:");
-                        for err in shunting_yard.errors {
-                            println!("ERR: {}", err);
-                        }
-                    } else {
-                        match shunting_yard.calculate() {
-                            Some(n) => {
-                                println!("Lexer result: {}", shunting_yard.to_string_ast());
-                                println!("Shunting Yard result: {}", shunting_yard.to_string());
-                                println!("Equation equals: {}", n);
-                            },
-                            None =>  {
-                                println!("Unable to calculate a result");
-                            }
-                        }
+            if shunting_yard.errors.len() > 0 {
+                println!("Errors:");
+                for err in shunting_yard.errors {
+                    println!("ERR: {}", err);
+                }
+            } else {
+                match shunting_yard.calculate() {
+                    Some(n) => {
+                        println!("Lexer result: {}", shunting_yard.to_string_ast());
+                        println!("Shunting Yard result: {}", shunting_yard.to_string());
+                        println!("Equation equals: {}", n);
+                    },
+                    None =>  {
+                        println!("Unable to calculate a result");
                     }
+                }
+            }
         },
         None => println!("Please supply an expression")
     };
