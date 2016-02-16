f8888f89df668c1d0eca3c6f974b17a7ee686682
Simon Whitehead <chemnova@gmail.com>
2016-02-16 22:37:05 +1100

    Return a Result<f64, Vec<String>> from calculate() so that consuming the calculate call is nicer

--- examples/main.rs
+++ examples/main.rs
@@ -9,20 +9,15 @@ fn main() {
 
             println!("Input is: {}", input);
 
-            if shunting_yard.errors.len() > 0 {
-                println!("Errors:");
-                for err in shunting_yard.errors {
-                    println!("ERR: {}", err);
-                }
-            } else {
-                match shunting_yard.calculate() {
-                    Some(n) => {
-                        println!("Lexer result: {}", shunting_yard.to_string_ast());
-                        println!("Shunting Yard result: {}", shunting_yard.to_string());
-                        println!("Equation equals: {}", n);
-                    },
-                    None =>  {
-                        println!("Unable to calculate a result");
+            match shunting_yard.calculate() {
+                Ok(n) => {
+                    println!("Lexer result: {}", shunting_yard.to_string_ast());
+                    println!("Shunting Yard result: {}", shunting_yard.to_string());
+                    println!("Equation equals: {}", n);
+                },
+                Err(errors) =>  {
+                    for err in errors {
+                        println!("ERR: {}", err);
                     }
                 }
             }
