9ad2001c83409cd3a5719e9e6f361f560467b290
Simon Whitehead <chemnova@gmail.com>
2016-03-03 17:27:10 +1100

    Added the full Wikipedia result assertion to the Wikipedia test

--- tests/wikipedia.rs
+++ tests/wikipedia.rs
@@ -8,6 +8,6 @@ extern crate rustyard;
 fn wikipedia_one() {
     let mut yard = rustyard::ShuntingYard::new();
 
-    yard.calculate("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3").unwrap();
+    assert_eq!(3.0001220703125f64, yard.calculate("3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3").unwrap());
     assert_eq!("3 4 2 * 1 5 - 2 3 ^ ^ / + ", yard.to_string());
 }
